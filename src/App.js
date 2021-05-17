2dd5822b14327be179986003206838478e95699a
John Doe <john@doe.org>
2021-05-17 22:10:21 +0200

    Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particales from "react-particles-js"
 import Nabvar from './components/Navbar';
+import Header from './components/Header';
 
 function App() {
   return (
-<Nabvar/>
-    );
+    <>
+    <Particales 
+    className= "particles-canvas"
+    parms={{
+      particles: {
+        number: {
+          value : 30,
+          density : {
+            enable : true,
+            value_area: 900
+          }
+        },
+        shape: {
+          type: "circle",
+          stroke: {
+            width: 6
+          }
+        }
+      }
+    }}
+    />
+      <Nabvar />
+      <Header />
+    </>
+  );  
 }
 
 export default App;
